4a35f683fa458c1bb0476fb060ccd1b9c9a98cca
ExTee <xintong.wang1995@gmail.com>
2017-12-14 18:04:46 -0500

    bug fix

--- conv_png.py
+++ conv_png.py
@@ -63,7 +63,8 @@ def normalizePlanes(npzarray):
 
 
 def main():
-	directory_path = './data/subset0/'  # path for mhd file
+	subset = 'subset0/'
+	directory_path = './data/' + subset  # path for mhd file
 	mhd_list = list_files(directory= directory_path)
 
 	for mhd_file in mhd_list:
@@ -81,23 +82,40 @@ def main():
 			# get candidates
 			# Extract and visualize patch for each candidate in the list
 			if cand[0] == img_path.replace(".mhd","").replace(directory_path, ""):
-				print('matched can and path')
+				print('matched candidate and path')
 				worldCoord = np.asarray([float(cand[3]), float(cand[2]), float(cand[1])])
 				voxelCoord = worldToVoxelCoord(worldCoord, numpyOrigin, numpySpacing)
+				
+				slice = int(voxelCoord[0])
+
+				coord_start = [0, 0, 0]
+				coord_end = [0, 512, 512]	
+
+				'''	
+				coord_start = [0, 0, 0]
+				coord_end = [0, 0, 0]
 				voxelWidth = 65
-				patch = numpyImage[voxelCoord[0], voxelCoord[1] - voxelWidth / 2: voxelCoord[1] + int(voxelWidth / 2),
-						voxelCoord[2] - voxelWidth / 2: voxelCoord[2] + voxelWidth / 2]
+				coord_start[1] = int(voxelCoord[1] - voxelWidth / 2.0)
+				coord_end[1] = int(voxelCoord[1] + voxelWidth / 2.0)
+				coord_start[2] = int(voxelCoord[2] - voxelWidth / 2.0)
+				coord_end[2] = int(voxelCoord[2] + voxelWidth / 2.0)
+				'''
+
+				patch = numpyImage[slice, coord_start[1]:coord_end[1], coord_start[2]:coord_end[2]]
+
 				patch = normalizePlanes(patch)
 
-				outputDir = './patches/'
-				plt.imshow(patch, cmap='gray')
-				plt.show()
+				outputDir = './patches/' + subset
+				#plt.imshow(patch, cmap='gray')
+				#plt.show()
 				#save the file as png format
 				Image.fromarray(patch * 255).convert('L').save(os.path.join(outputDir,
 																			'patch_' + str(worldCoord[0]) + '_' + str(
 																				worldCoord[1]) + '_' + str(
 																				worldCoord[2]) + '.png'))
 				print('File Saved')
+
+	print('All files save in ' + outputDir)
 
 main()
 
